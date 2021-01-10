9cb7c178885a7c5681cbdadd48c241e0cf16d993
Hardik Chauhan <hardikidea@gmail.com>
2021-01-10 23:42:04 +0530

    WebMenu master added

---
+++ server/migrations/20210110175742-create-web-menu-master.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('WebMenuMasters', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      header: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      tooltip: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      routeURL: {
+        type: Sequelize.STRING,
+      },
+      displayOrder: {
+        type: Sequelize.INTEGER
+      },
+      parentId: {
+        type: Sequelize.INTEGER
+      },
+      isNonMenuPage: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('WebMenuMasters');
+  }
+};
---
+++ server/models/webmenumaster.js
@@ -0,0 +1,78 @@
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class WebMenuMaster extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   WebMenuMaster.init({
+//     name: DataTypes.STRING,
+//     header: DataTypes.STRING,
+//     title: DataTypes.STRING,
+//     tooltip: DataTypes.STRING,
+//     routeURL: DataTypes.STRING,
+//     displayOrder: DataTypes.INTEGER,
+//     parentId: DataTypes.INTEGER,
+//     isNonMenuPage: DataTypes.BOOLEAN
+//   }, {
+//     sequelize,
+//     modelName: 'WebMenuMaster',
+//   });
+//   return WebMenuMaster;
+// };
+
+export default (sequelize, DataTypes) => {
+  const WebMenuMaster = sequelize.define('WebMenuMaster', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    header: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    tooltip: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    routeURL: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    displayOrder: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    parentId: {
+      type: DataTypes.INTEGER,
+    },
+    isNonMenuPage: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: true
+    }
+  });
+
+  WebMenuMaster.associate = (models) => {
+    // // WEB-MENU-MASTER
+    // WebMenuMaster.belongsTo(models.WebMenuMaster, {
+    //   // foreignKey: 'id',
+    //   // key: 'WebMenuMaster'
+    //   // targetKey: 'name'
+    // });
+  };
+
+  return WebMenuMaster;
+};
